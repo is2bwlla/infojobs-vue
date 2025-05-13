2f4daadd410b2a1103afac19e90f21afbdc82d97
is2bwlla <lucyrose22198@gmail.com>
2025-05-13 16:52:45 -0300

    login e signup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
-
+import { useRouter } from 'vue-router';
 
 const username = ref();
 const password = ref();
+
+const router = useRouter();
 
 const logar =() => {
   axios.post('https://dummyjson.com/user/login', {
@@ -19,16 +21,20 @@ const logar =() => {
   })
 }
 
+const cadastrar = () => {
+  router.push('/signup');
+}
+
 </script>
 
 <template>
   <main class="flex justify-center items-center w-screen h-screen">
-    <div class="flex flex-col gap-2">
-      <h1 class="font-bold text-3xl">CREDENCIAIS</h1>
-      <input type="text" class="border rounded p-1.5" v-model="username">
-      <input type="text" class="border rounded p-1.5" v-model="password">
-      <button @click="logar" class="border border-pink-300 bg-pink-300 rounded p-2 text-white font-bold">LOGIN</button>
+    <div class="flex flex-col gap-2 items-center">
+      <h1 class="font-bold text-2xl">LOGIN</h1>
+      <input type="text" class="border rounded p-1.5 border-gray-500" placeholder="Ex: Igor Bedon" v-model="username">
+      <input type="password" class="border rounded p-1.5 border-gray-500" placeholder="password" v-model="password">
+      <button @click="logar" class="border border-pink-300 bg-pink-300 rounded p-2 text-white font-bold w-full">LOGIN</button>
+      <button @click="cadastrar" class="border-pink-300 border-2 rounded font-bold p-2 text-gray-500 w-full">CADASTRAR</button>
     </div>
-    
   </main>
 </template>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const username = ref('');
+const email = ref('');
+const firstName = ref('');
+const lastName = ref('');
+const gender = ref('');
+const age = ref('');
+const password = ref('');
+const router = useRouter();
+
+const logar = () => {
+  axios.post('https://dummyjson.com/users/add', {
+    username: username.value,
+    email: email.value,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    gender: gender.value,
+    age: age.value,
+    password: password.value
+  }).then((res) => {
+    console.log(res)
+  }).catch((e) => {
+    console.log(e)
+  }).finally(() => {
+    console.log("Acabou.")
+  })
+}
+
+const login = () => {
+  router.push('/');
+}
+</script>
+
+<template>
+    <main class="flex justify-center items-center w-screen h-screen bg-gray-100">
+      <div class="flex flex-col gap-6 p-6 bg-white shadow-lg rounded-lg w-full sm:w-96">
+        <h1 class="font-bold text-2xl text-center text-gray-800">CADASTRO</h1>
+        <form @submit.prevent="logar" class="flex flex-col gap-3">
+  
+          <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div class="flex flex-col w-auto">
+              <label for="username">Nome de usuário:</label>
+              <input type="text" class="border rounded p-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: igorbedon" v-model="username" required/>
+            </div>
+
+            <div class="flex flex-col">
+              <label for="firstName">Primeiro nome:</label>
+              <input type="text" class="border rounded p-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: Igor" v-model="firstName" required/>
+            </div>
+          </div>
+
+          <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div class="flex flex-col">
+              <label for="lastName">Sobrenome</label>
+              <input type="text" class="border rounded p-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: Bedon" v-model="lastName" required/>
+            </div>
+            
+            <div class="flex flex-col">
+              <label for="password">Senha:</label>
+              <input type="password" class="border rounded p-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: amoaisabella" v-model="password" required/>
+            </div>
+          </div>
+  
+          <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div class="flex flex-col">
+              <label for="gender">Gênero:</label>
+              <input type="text" class="border rounded p-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: masculino" v-model="gender" required/>
+            </div>
+            
+            <div class="flex flex-col">
+              <label for="age">Idade:</label>
+              <input type="number" class="border rounded p-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: 20" v-model="age" required/>
+            </div>
+          </div>
+  
+          <div class="grid grid-cols-1 gap-4">
+            <div class="flex flex-col">
+              <label for="email">email</label>
+              <input type="email" class="border rounded p-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: igor@example.com" v-model="email" required/>
+            </div>
+          </div>
+          <button type="submit" class="bg-pink-300 text-white p-3 rounded-md font-semibold hover:bg-pink-400 transition">CADASTRAR</button>
+        </form>
+        
+        <button @click="login" class="border-2 border-pink-300 text-gray-500 p-3 rounded-md font-semibold hover:border-pink-400 transition">LOGAR</button>
+      </div>
+    </main>
+  </template>
