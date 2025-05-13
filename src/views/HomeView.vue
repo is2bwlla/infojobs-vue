0eb4f0dfb2b6acd2987656884e57ee8d016b3943
is2bwlla <lucyrose22198@gmail.com>
2025-05-13 14:27:56 -0300

    'jobs'

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+
+const username = ref();
+const password = ref();
+
+const logar =() => {
+  axios.post('https://dummyjson.com/user/login', {
+    username: username.value,
+    password: password.value
+  }).then((res) => {
+    console.log(res)
+  }).catch((e) => {
+    console.log(e)
+  }).finally(() => {
+    console.log("Acabou.")
+  })
+}
+
+</script>
+
+<template>
+  <main class="flex justify-center items-center w-screen h-screen">
+    <div class="flex flex-col gap-2">
+      <h1 class="font-bold text-3xl">CREDENCIAIS</h1>
+      <input type="text" class="border rounded p-1.5" v-model="username">
+      <input type="text" class="border rounded p-1.5" v-model="password">
+      <button @click="logar" class="border border-pink-300 bg-pink-300 rounded p-2 text-white font-bold">LOGIN</button>
+    </div>
+    
+  </main>
+</template>
